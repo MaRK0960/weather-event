2c415612deb7459f239ea10262f89555fd6bae8c
MaRK0960 <ahmedhesham0960@outlook.com>
2024-11-07 15:58:21 +0200

    start container every 5 minutes

--- WeatherTimer.cs
+++ WeatherTimer.cs
@@ -1,42 +1,23 @@
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.ContainerInstance;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
-using System.Net;
-using System.Net.Mail;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace weather_event
 {
     public class WeatherTimer
     {
         [FunctionName("WeatherTimer")]
-        public void Run([TimerTrigger("0 0 * * * *")] TimerInfo myTimer, ILogger log)
+        public void Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer, ILogger log)
         {
-            SendEmail("***REMOVED***", "Test Timer", "Test Timer").Wait();
-        }
+            ArmClient client = new(new DefaultAzureCredential());
 
-        private static async Task SendEmail(string toAddress, string subject, string body)
-        {
-            MailMessage mail = new()
-            {
-                BodyEncoding = Encoding.UTF8,
-                SubjectEncoding = Encoding.UTF8,
-                From = new MailAddress("***REMOVED***"),
-                Subject = subject,
-                Body = body
-            };
+            ContainerGroupResource containerGroup = client.GetContainerGroupResource(
+                ResourceIdentifier.Parse("***REMOVED***"));
 
-            mail.To.Add(toAddress);
-            mail.Headers.Add("Content-Type", "text/html; charset=utf-8");
-
-            SmtpClient smtpClient = new("smtp.azurecomm.net", 587)
-            {
-                Credentials = new NetworkCredential("***REMOVED***", "***REMOVED***"),
-                EnableSsl = true,
-                DeliveryMethod = SmtpDeliveryMethod.Network
-            };
-
-            await smtpClient.SendMailAsync(mail);
+            containerGroup.Start(Azure.WaitUntil.Started);
         }
     }
 }
