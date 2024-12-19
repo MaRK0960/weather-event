41533b1ead7acfea6a387a55a838af2fa40a27c8
MaRK0960 <ahmedhesham0960@outlook.com>
2024-12-19 21:17:49 +0200

    remove logging

--- WeatherUpdateSubscription.cs
+++ WeatherUpdateSubscription.cs
@@ -20,8 +20,6 @@ namespace weather_event
                 .SelectMany(es => es.Select(e => e.Data.ToObjectFromJson<string>()))
                 .ToList();
 
-            log.LogInformation(string.Join(',', emails));
-
             string emailBody = EmailTemplate.Get("weather-modified-email-template.html");
 
             emailBody = emailBody.Replace("{SiteURL}", AppConfiguration.Get("Weather:Site"));
