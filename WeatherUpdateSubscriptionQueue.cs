de6cc8b6eaa6a5cfaff79d7bdfeb5988db0ce165
MaRK0960 <ahmedhesham0960@outlook.com>
2024-12-22 17:31:38 +0200

    deserialize and send email

--- WeatherUpdateSubscriptionQueue.cs
+++ WeatherUpdateSubscriptionQueue.cs
@@ -1,14 +1,37 @@
+using Azure.Messaging.EventGrid;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+using weather_event.Service;
 
 namespace weather_event
 {
     public class WeatherUpdateSubscriptionQueue
     {
         [FunctionName("WeatherUpdateSubscriptionQueue")]
-        public void Run([QueueTrigger("weather-modified-subscription", Connection = "AzureWebJobsStorage")] string myQueueItem, ILogger log)
+        public Task Run([QueueTrigger("weather-modified-subscription", Connection = "AzureWebJobsStorage")] string myQueueItem, ILogger log)
         {
-            log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+            EventGridEvent eventGridEvent = JsonSerializer.Deserialize<EventGridEvent>(myQueueItem);
+
+            string email = eventGridEvent.Data.ToObjectFromJson<string>();
+
+            string emailBody = EmailTemplate.Get("weather-modified-email-template.html");
+
+            emailBody = emailBody.Replace("{SiteURL}", AppConfiguration.Get("Weather:Site"));
+
+            string emailSubject = "7'tfa Weather Notification App Subscription Updated!";
+
+            try
+            {
+                return EmailSender.Send(email, emailSubject, emailBody);
+            }
+            catch (Exception x)
+            {
+                log.LogCritical(x, "Error in SendEmail");
+                return Task.FromException<Exception>(x);
+            }
         }
     }
 }
